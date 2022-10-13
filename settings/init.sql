0912951585a8c627abeb0ba626ec42ae990a214a
Arun Sethia <109227248+sethiaarun@users.noreply.github.com>
2022-10-13 14:07:02 -0500

    commented hive.optimize.sort.dynamic.partition.threshold

--- settings/init.sql
+++ settings/init.sql
@@ -21,4 +21,4 @@ set hive.auto.convert.join.shuffle.max.size=10000000000;
 set hive.tez.llap.min.reducer.per.executor=0.33;
 set hive.map.aggr.hash.min.reduction=0.99;
 
-set hive.optimize.sort.dynamic.partition.threshold=0;
+--set hive.optimize.sort.dynamic.partition.threshold=0;
